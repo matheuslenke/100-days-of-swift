e7b0be6c96b51f46c2ff79cc4c54d6f3d4f718e9
Matheus Lenke <matheus.l.coutinho@aluno.ufes.br>
2021-05-21 22:29:30 -0300

    Day 1 done

---
+++ Day1/Variables.playground/Contents.swift
@@ -0,0 +1,45 @@
+import UIKit
+
+// Strings
+var greeting = "Hello, playground"
+
+// Integers
+var age: Int = 23
+
+// Multi-line Strings
+
+var str1 = """
+this goes
+over multiple
+lines
+"""
+
+// Multi-line without line breaks in string
+var str2 = """
+this goes \
+over multiple \
+lines
+"""
+
+// Double
+// In swift it is double by default in hidden type declaration
+var pi = 3.141
+
+// Bool
+var yes = true
+var no = false
+
+// String interpolation
+var score = 90
+var text = "Your score was \(score)"
+var text2 = "Content of text: \(text)"
+
+// Constants
+let taylor = "swift"
+
+// Type Annotations
+let str: String = "Test"
+let number: Int = 10
+let floating: Float = 3.14
+let double: Double = 2.40
+let boolean: Bool = true
