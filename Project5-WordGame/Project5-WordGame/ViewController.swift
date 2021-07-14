b46130a4aab7bdbc4270991569cf94921d1d1306
Matheus Lenke <matheus.l.coutinho@aluno.ufes.br>
2021-07-14 08:28:08 -0300

    Day 28 of 100 done

--- Project5-WordGame/Project5-WordGame/ViewController.swift
+++ Project5-WordGame/Project5-WordGame/ViewController.swift
@@ -60,7 +60,69 @@ class ViewController: UITableViewController {
     }
     
     func submit(_ answer: String) {
+        let lowerAnswer = answer.lowercased()
         
+        let errorTitle: String
+        let errorMessage: String
+        
+        if !isSameWord(word: lowerAnswer) {
+            if isPossible(word: lowerAnswer) {
+                if isOriginal(word: lowerAnswer) {
+                    if isReal(word: lowerAnswer) {
+                        usedWords.insert(answer, at: 0)
+                        
+                        let indexPath = IndexPath(row: 0, section: 0)
+                        tableView.insertRows(at: [indexPath], with: .automatic)
+                        return
+                    } else {
+                        errorTitle = "Word not recognized"
+                        errorMessage = "You can't just make them up, you know!"
+                    }
+                } else {
+                    errorTitle = "Word already used"
+                    errorMessage = "Be more original!!"
+                }
+            } else {
+                errorTitle = "Word not possible"
+                errorMessage = "You can't spell that word form \(title!.lowercased())"
+            }
+        } else {
+            errorTitle = "This is the title word!"
+            errorMessage = "You can't use \"\(title!.lowercased())\" again!"
+        }
+        
+        let ac = UIAlertController(title: errorTitle, message: errorMessage, preferredStyle: .alert)
+        ac.addAction(UIAlertAction(title: "Ok", style: .default))
+        present(ac, animated: true)
+    }
+    
+    func isPossible(word: String) -> Bool {
+        guard var tempWord = title?.lowercased() else { return false }
+        
+        for letter in word {
+            if let position = tempWord.firstIndex(of: letter) {
+                tempWord.remove(at: position)
+            } else {
+                return false
+            }
+        }
+        return true
+    }
+    
+    func isOriginal(word: String) -> Bool {
+        return !usedWords.contains(word)
+    }
+    
+    func isReal(word: String) -> Bool {
+        let checker = UITextChecker()
+        let range = NSRange(location: 0, length: word.utf16.count)
+        let misspelledRange = checker.rangeOfMisspelledWord(in: word, range: range, startingAt: 0, wrap: false, language: "en")
+        return misspelledRange.location == NSNotFound
+    }
+    
+    func isSameWord(word: String) -> Bool {
+        guard let tempWord = title?.lowercased() else { return false }
+        return word == tempWord
     }
 }
 
--- README.md
+++ README.md
@@ -2,4 +2,6 @@
 
 ### Logs
 
-**Day 26:** Today i learned a little about Capture lists in swift, and undertood the difference between weak, unowned and strong references, what they mean and how to write safe code preventing crashed and memory leaks
+**Day 27:** Today i learned a little about Capture lists in swift, and undertood the difference between weak, unowned and strong references, what they mean and how to write safe code preventing crashed and memory leaks
+
+**Day 28** Today i learned a little about comparing strings and checking if a word is real, is possible, or it's letters are contained in another word. I also learned that in Swift you cant compare 2 strings with the == operator, just like booleans and numbers
