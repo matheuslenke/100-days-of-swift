4695462bceb2e548e32524f6ce61de95f67cba77
Matheus Lenke <matheus.l.coutinho@aluno.ufes.br>
2021-07-17 20:06:33 -0300

    Day 33 of 100 done

---
+++ Project7-DataFeed/Project7-DataFeed/Petition.swift
@@ -0,0 +1,16 @@
+//
+//  Petition.swift
+//  Project7-DataFeed
+//
+//  Created by Matheus Lenke on 17/07/21.
+//
+
+import Foundation
+
+struct Petition: Codable {
+    var title: String
+    var body: String
+    var signatureCount: Int
+}
+
+
---
+++ Project7-DataFeed/Project7-DataFeed/Petitions.swift
@@ -0,0 +1,12 @@
+//
+//  Petitions.swift
+//  Project7-DataFeed
+//
+//  Created by Matheus Lenke on 17/07/21.
+//
+
+import Foundation
+
+struct Petitions: Codable {
+    var results: [Petition]
+}
