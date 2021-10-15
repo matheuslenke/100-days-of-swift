85bf92bfa242d9fdafec660b929eac439fed0abc
Matheus Lenke <matheuslenke@gmail.com>
2021-10-14 21:14:53 -0300

    Day 83 of 100 done

---
+++ Project25-P2Photos/Project25-P2Photos/AppDelegate.swift
@@ -0,0 +1,23 @@
+//
+//  AppDelegate.swift
+//  Project25-P2Photos
+//
+//  Created by Matheus Lenke on 14/10/21.
+//
+
+import UIKit
+
+@main
+class AppDelegate: UIResponder, UIApplicationDelegate {
+
+    var window: UIWindow?
+
+    func application(_ application: UIApplication, didFinishLaunchingWithOptions launchOptions: [UIApplication.LaunchOptionsKey: Any]?) -> Bool {
+        // Override point for customization after application launch.
+        return true
+    }
+
+
+
+}
+
